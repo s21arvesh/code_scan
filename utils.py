79c489853a0d99620fbfeed373c04a992460ca26
s21arvesh <131661674+s21arvesh@users.noreply.github.com>
2026-02-07 13:33:57 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,13 +1,26 @@
-def calculate_total(price, tax):
-    total = price + tax
-    if total > 100:
-        total = total - 10
-    return total
+def complex_function(a, b, c):
+    result = 0
+    if a > 0:
+        if b > 0:
+            if c > 0:
+                result += 1
+            else:
+                result -= 1
+        else:
+            if c > 0:
+                result += 2
+            else:
+                result -= 2
+    else:
+        if b > 0:
+            result += 3
+        else:
+            result -= 3
 
+    for i in range(10):
+        if i % 2 == 0:
+            result += i
+        else:
+            result -= i
 
-def calculate_invoice(price, tax):
-    # ❌ duplicated logic (radon + maintainability)
-    total = price + tax
-    if total > 100:
-        total = total - 10
-    return total
+    return result
